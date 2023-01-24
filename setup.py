7eaccdb91262ef109a9b1d7a6176c04208cc6218
Oscar Branson <oscarbranson@gmail.com>
2023-01-24 18:05:05 +0000

    switch over to using setup.cfg

--- setup.py
+++ setup.py
@@ -1,22 +1,3 @@
 from setuptools import setup, find_packages
 
-setup(name='brutefit',
-      version='0.0.1',
-      description='Tools for finding an arbitrary multivariate polynomial that best fits some data.',
-      url='https://github.com/oscarbranson/brutefit',
-      author='Oscar Branson',
-      author_email='oscarbranson@gmail.com',
-      license='MIT',
-      packages=find_packages(),
-      keywords=['science', 'data', 'empiricism'],
-      classifiers=['Development Status :: 4 - Beta',
-                   'Intended Audience :: Science/Research',
-                   'Programming Language :: Python :: 3 :: Only',
-                   ],
-      install_requires=['numpy',
-                        'sklearn',
-                        'pandas',
-                        'sympy',
-                        'tqdm'],
-      # package_data={'cbsyst': ['test_data/*']},
-      zip_safe=True)
+setup(packages=find_packages())
